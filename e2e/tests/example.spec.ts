c3793957545d90e7d2e95c628d78f1e1250e5389
tux7P <55027789+tux7P@users.noreply.github.com>
2024-05-09 01:47:24 +0300

    Add registration raw tests

--- e2e/tests/example.spec.ts
+++ e2e/tests/example.spec.ts
@@ -26,3 +26,32 @@ test('new test', async ({page}) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', {name: 'Installation'})).toBeVisible();
 });
+
+test('CV Online registration', async ({page}) => {
+  await page.goto('https://test.cv.ee/en');
+
+  await page.getByRole('button', {name: 'Login | Register'}).click();
+
+  await page.getByRole('button', {name: 'Registration'}).click();
+  await page.getByRole('button', {name: 'For job seekers'}).click();
+  await page.getByRole('button', {name: 'Register with email'}).click();
+
+  await page.getByLabel('Email address').click();
+  await page.getByLabel('Email address').fill('demo@test.com');
+
+  await page.getByLabel('PasswordIcons / 16 / input').click();
+  await page.getByLabel('PasswordIcons / 16 / input').fill('23432423');
+
+  await page.getByLabel('First name').click();
+  await page.getByLabel('First name').fill('Demo');
+  await page.getByLabel('First name').press('Tab');
+
+  await page.getByLabel('Last name').fill('Test');
+
+  await page.locator('label').filter({hasText: 'I want to receive CV-Online'}).locator('span').first().click();
+  await page.locator('label').filter({hasText: 'I agree to the Terms of use'}).locator('span').first().click();
+  await page.locator('label').filter({hasText: 'I agree that my information'}).locator('span').first().click();
+
+  await page.getByRole('button', {name: 'Register', exact: true}).click();
+  // await expect(page).toHaveTitle(/Playwright/);
+});
