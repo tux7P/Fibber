6cfc81828e5367ac339ffccfb98d6906ecb57191
tux7P <55027789+tux7P@users.noreply.github.com>
2024-05-21 00:38:10 +0300

    Remove redundant step

--- tests/register-ui.spec.ts
+++ tests/register-ui.spec.ts
@@ -7,32 +7,29 @@ test.describe(`UI - Registration Tests ${tags.TestType.Regression}  ${tags.TestT
     await navigation.openHomePage();
   });
 
-  test('Verify user can register successfully through UI', async ({navigation, registrationModal}) => {
+  test('Verify user can register successfully through UI', async ({registrationModal}) => {
     const email: string = Generator.generateEmail();
     const password = Generator.generatePassword();
     const firstName = Generator.generateFirstName();
     const lastName = Generator.generateLastName();
 
-    await navigation.openHomePage();
     await registrationModal.goToRegistrationForm();
     await registrationModal.registerNewUser(email, password, firstName, lastName);
     await registrationModal.verifyRegistrationIsSuccessful();
   });
 
-  test('Verify duplicate email error through UI', async ({navigation, registrationModal}) => {
+  test('Verify duplicate email error through UI', async ({registrationModal}) => {
     const duplicateEmail: string = 'test@test.com';
     const password = Generator.generatePassword();
     const firstName = Generator.generateFirstName();
     const lastName = Generator.generateLastName();
 
-    await navigation.openHomePage();
     await registrationModal.goToRegistrationForm();
     await registrationModal.registerNewUser(duplicateEmail, password, firstName, lastName);
     await registrationModal.verifyDuplicateEmailErrorIsDisplayed();
   });
 
   test('Verify minimum password throws invalid parameter error for password field in UI', async ({
-    navigation,
     registrationModal,
   }) => {
     const email: string = Generator.generateEmail();
@@ -40,17 +37,12 @@ test.describe(`UI - Registration Tests ${tags.TestType.Regression}  ${tags.TestT
     const firstName = Generator.generateFirstName();
     const lastName = Generator.generateLastName();
 
-    await navigation.openHomePage();
     await registrationModal.goToRegistrationForm();
     await registrationModal.registerNewUser(email, password, firstName, lastName);
     await registrationModal.verifyMinimumPasswordErrorIsDisplayed();
   });
 
-  test('Verify user can close registration modal without being stuck in UI', async ({
-    navigation,
-    registrationModal,
-  }) => {
-    await navigation.openHomePage();
+  test('Verify user can close registration modal without being stuck in UI', async ({registrationModal}) => {
     await registrationModal.goToRegistrationForm();
     await registrationModal.closeRegistrationModal();
     await registrationModal.verifyRegistrationModalIsClosed();
